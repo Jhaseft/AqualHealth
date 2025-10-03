98d5dc695bf4f6f29c6a4e0793e6ad3032a3d863
Jhaseft <jhasesaat@gmail.com>
2025-10-03 02:58:22 -0400

    app servide provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
-   public function boot(): void
+    public function boot(): void
     {
         // Forzar URLs con HTTPS en producción
         if ($this->app->environment('production')) {
