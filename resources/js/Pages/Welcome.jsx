a0241d50d0391c03689d94bea6224e10120f5274
Jhaseft <jhasesaat@gmail.com>
2025-10-03 01:06:30 -0400

    first commit

---
+++ resources/js/Pages/Welcome.jsx
@@ -0,0 +1,15 @@
+import { Head, Link } from '@inertiajs/react';
+
+export default function Welcome() {
+    
+
+    return (
+        <>
+           <div className='flex justify-center items-center h-screen'>
+                <p className='text-3xl text-center font-bold underline text-black'>
+                    test
+                </p>
+           </div>
+        </>
+    );
+}
